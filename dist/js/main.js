4769f8e86dec545c33b133e3ad1233233f6fee98
Abedy <abedy.ewu@gmail.com>
2019-09-22 11:19:10 +0600

    index h1

--- dist/js/main.js
+++ dist/js/main.js
@@ -145,10 +145,12 @@ for (var i = 0; i < columns; i++) {
 // Setting up the draw function
 function draw() {
   ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+  // ctx.fillStyle = '#ff0000' 
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   for (var i = 0; i < drops.length; i++) {
     var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
+    // ctx.fillStyle = '#0f0';
+    ctx.fillStyle = '#ff0000';
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
     if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
